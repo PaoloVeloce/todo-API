9f2cd695723b84fdeb6a358dd76aaf24d9ca0ba6
Paolo Veloce <pablo.veloce@gmail.com>
2016-06-29 17:58:30 +0300

    initial commit with simple server tuned

---
+++ server.js
@@ -0,0 +1,16 @@
+
+// import express module
+var express = require('express');
+var app = express();
+// make PORT variable heroku ready
+var PORT = process.env.PORT || 3000;
+
+// add a root directory with text To Do API Root
+app.get('/', function (req, res) {
+	res.send('To Do API root');
+});
+
+// starting webserver and console.log text with server runnig message
+app.listen(PORT, function(req, res) {
+	console.log('Express is listening at port ' + PORT + '!');
+})

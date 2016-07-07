a33b27a9ed63c2a7c029a9b60eb26d1c13598e01
Paolo Veloce <pablo.veloce@gmail.com>
2016-07-07 18:07:54 +0300

    add body parser and and 'id adding' functionality

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 
 // import express module
 var express = require('express');
+// import bodyParser middleware = "software glue"
+var bodyParser = require('body-parser');
 var app = express();
 // make PORT variable heroku ready
 var PORT = process.env.PORT || 3000;
 // store all todo items
-var todos =[{
-	id: 1, 
-	description: 'Meet wife for lunch',
-	completed: false
-}, {
-	id: 2,
-	descriprion: 'Follow the white rabbit',
-	completed: false
-}, {
-	id: 3, 
-	description: 'Go to sauna',
-	completed: true
-}];
+var todos =[];
+var todoNextId = 1;
+
+// setupping middleware - when json parsed we have access to it in json.body
+app.use(bodyParser.json());
+
 
 // add a root directory with text To Do API Root
 app.get('/', function (req, res) {
@@ -49,6 +44,23 @@ app.get('/todos/:id', function( req, res) {
 	
 	res.send('Asking for todo with id of ' + req.params.id)
 });
+
+// POST /todos/:id
+app.post('/todos', function (req, res) {
+	// create variable 
+	var body = req.body;
+	
+	// add id field
+	body.id = todoNextId;
+	todoNextId++;
+	// push body into array
+	todos.push(body);
+	
+	console.log('description: ' + body.description);
+	// 
+	res.json(body);
+});
+
 
 
 // starting webserver and console.log text with server runnig message

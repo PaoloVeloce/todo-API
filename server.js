5770188d9b7b6c7172e2a13370ee1cbe96b307f5
Paolo Veloce <pablo.veloce@gmail.com>
2016-07-11 18:21:26 +0300

    add delete todos route

--- server.js
+++ server.js
@@ -72,6 +72,19 @@ app.post('/todos', function (req, res) {
 	res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with this id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
+
 
 
 // starting webserver and console.log text with server runnig message
